c3ac3f3a35ec8540d7766a6d178451d03cdf230f
Luccas <luccasschepis@outlook.com>
2024-04-27 20:18:42 -0300

    The calculator is now functional

--- src/script.js
+++ src/script.js
@@ -1,2 +1,65 @@
-const botões = document.querySelectorAll('.button')
+const Display = document.getElementById("display");
+const TeclasNumericas = document.querySelectorAll(".num");
+const TeclasOperacionais = document.querySelectorAll(".operation");
+const clear = document.querySelectorAll(".AC");
+const invert = document.querySelectorAll(".plusminus");
+const resultado = document.querySelectorAll(".result");
 
+let sinal = false
+
+TeclasNumericas.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        if (Display.innerHTML == "0") {
+            Display.innerHTML = "0,";
+        }
+    else {
+            Display.innerHTML += evt.target.innerHTML;
+        }
+    });
+});
+
+TeclasOperacionais.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        if (sinal == false) {
+            sinal = true;
+            switch(evt.target.innerHTML) {
+                case "×":
+                    Display.innerHTML += "*";
+                    break;
+                case ".":
+                    Display.innerHTML += ",";
+                    break;
+                case "÷":
+                    Display.innerHTML += "/";
+                    break;
+                default:
+                    Display.innerHTML += evt.target.innerHTML;
+            }
+        }
+    });
+    sinal = false;
+});
+
+clear.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        sinal = false
+        Display.innerHTML = "0";
+    });
+});
+invert.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        sinal = false
+        if (Display.innerHTML !== "0") {
+            let num = parseFloat(Display.innerHTML);
+            num *= -1;
+            Display.innerHTML = num.toString();
+        }
+    });
+});
+resultado.forEach((el) => {
+    el.addEventListener("click", (evt) => {
+        sinal = false
+        const res = eval(Display.innerHTML);
+        Display.innerHTML = res;
+    });
+});
